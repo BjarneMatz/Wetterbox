9d2452a6130a2c1e411bd0d1bd9d4d03cfeb6af2
BjarneMatz <bjarne.matz@gmail.com>
2023-04-30 01:12:29 +0200

    added more documentation

--- RaspberryPi/readserial.py
+++ RaspberryPi/readserial.py
@@ -1,31 +1,30 @@
-# read serial data from arduino output and save to file via database skript on backend server
-
+#import needed libraries
 from serial import Serial
 import time
 import json
-import datetime
-import os
+
 
 def read_serial():
     """read serial data from arduino"""
-    ser = Serial('COM8', 9600, timeout=1)
-    ser.flush()
+    ser = Serial('COM8', 9600, timeout=1) #initialize serial connection
+    ser.flush() #flush serial buffer
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            send_data(line)
-            ser.write(b"1")
+        if ser.in_waiting > 0: #if there is data in the serial buffer
+            line = ser.readline().decode('utf-8').rstrip() #read the data
+            send_data(line) #send the data to the server
+            ser.write(b"1") #send some data to the arduino to tell it the connection is still alive
         
 
-
 def send_data(data):
-    """send data to backend server"""
+    """send data to server
+    data: string"""
     print(data)
-    pass
+
 
 def main():
     """main function"""
     read_serial()
+
 
 if __name__ == "__main__":
     main()
