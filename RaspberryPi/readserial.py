f7a9e9cf10ba6ab81829d6c3a7a5c51e41f32f94
BjarneMatz <bjarne.matz@gmail.com>
2023-04-30 00:41:27 +0200

    basic reading function for raspberry

---
+++ RaspberryPi/readserial.py
@@ -0,0 +1,28 @@
+# read serial data from arduino output and save to file via database skript on backend server
+
+from serial import Serial
+import time
+import json
+import datetime
+import os
+
+def read_serial():
+    """read serial data from arduino"""
+    ser = Serial('COM3', 9600, timeout=1)
+    ser.flush()
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            send_data(line)
+        
+def send_data(data):
+    """send data to backend server"""
+    print(data)
+    pass
+
+def main():
+    """main function"""
+    read_serial()
+
+if __name__ == "__main__":
+    main()
